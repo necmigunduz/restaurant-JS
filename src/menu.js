2b0f77d403c4583c35fa0f1e22bc7c1dff8a9715
necmigunduz <necmigunduz@gmail.com>
2021-04-14 18:45:56 +0300

    Fix Eslint

--- src/menu.js
+++ src/menu.js
@@ -23,33 +23,13 @@ function Menu() {
   const spaceIII = document.createElement('div');
   spaceIII.setAttribute('class', 'space');
 
-  starters.innerHTML = `<span><strong>Starters: </strong></span>
-    <span>Salty Pastries</span>,
-    <span>Cheese Plate</span>,
-    <span>Chef's Special Soup of Day</span>,
-    <span>Beverages and Spirits</span>`;
-  spaceI.innerHTML = `<br /><br />`;
-  fishes.innerHTML = `<span><strong>Fishes: </strong></span>
-    <span>Sea Trout</span>,
-    <span>Octopus and Calamar</span>,
-    <span>Cat Fish</span>,
-    <span>Whitey</span>,
-    <span>Branzino</span>`;
+  starters.innerHTML = '<span><strong>Starters: </strong></span><span>Salty Pastries</span><span>Cheese Plate</span>,<span>Chefs Special Soup of Day</span>,<span>Beverages and Spirits</span>';
+  spaceI.innerHTML = '<br /><br />';
+  fishes.innerHTML = '<span><strong>Fishes: </strong></span><span>Sea Trout</span>,<span>Octopus and Calamar</span>,<span>Cat Fish</span>,<span>Whitey</span>,<span>Branzino</span>';
   spaceII.innerHTML = '<br /><br />';
-  salads.innerHTML = `<span><strong>Salads: </strong></span>
-    <span>Season Salad</span>,
-    <span>Onion Salad</span>,
-    <span>Greens Salad</span>,
-    <span>Seafood Salad</span>,
-    <span>Cheese Salad</span>,
-    <span>Mediterrenian Salad</span>`;
-  spaceIII.innerHTML = `<br > <br/>`;
-  desserts.innerHTML = `<span><strong>Desserts: </strong></span>
-    <span>Baked Halva</span>,
-    <span>Milk Pudding</span>,
-    <span>Baklava</span>,
-    <span>Fruit Yoghurd Kinds</span>,
-    <span>Kunefe</span>`;
+  salads.innerHTML = '<span><strong>Salads: </strong></span><span>Season Salad</span>,<span>Onion Salad</span>,<span>Greens Salad</span>,<span>Seafood Salad</span>,<span>Cheese Salad</span>,<span>Mediterrenian Salad</span>';
+  spaceIII.innerHTML = '<br > <br/>';
+  desserts.innerHTML = '<span><strong>Desserts: </strong></span><span>Baked Halva</span>,<span>Milk Pudding</span>,<span>Baklava</span>,<span>Fruit Yoghurd Kinds</span>,<span>Kunefe</span>';
 
   menuDiv.appendChild(starters);
   menuDiv.appendChild(spaceI);

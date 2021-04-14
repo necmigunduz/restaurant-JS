1d60e6b8f845b352020ce52d4e8ddbe0f5e97d8b
necmigunduz <necmigunduz@gmail.com>
2021-04-14 15:25:20 +0300

    Add contact

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,10 @@
-function contact {
+function contact() {
+    const contact_div = document.createElement('div');
+    contact_div.setAttribute('class','flex-column');
+
+    contact_div.innerHTML = `<span>To send your feedbacks about your experience in our restaurant:</span><span>myexperience@blueocean.com</span><span>For general information requests:</span><span>info@blueocean.com</span>`;
+
+    return contact_div;
 }
 
 export default contact;

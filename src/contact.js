3afd7845c73163286fb5645eacaedf854da8a1f4
necmigunduz <necmigunduz@gmail.com>
2021-04-14 18:13:11 +0300

    Fix Eslint

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,10 @@
 function Contact() {
-  const contact_div = document.createElement("div");
-  contact_div.setAttribute("class", "flex-column, contact");
+  const contactDiv = document.createElement('div');
+  contactDiv.setAttribute('class', 'flex-column, contact');
 
-  contact_div.innerHTML = `<span><strong>To send your feedbacks about your experience in our restaurant: </strong></span><br /><br><span>myexperience@blueocean.com</span><br /><br><span><strong>For general information requests: </strong></span><br /><br><span>info@blueocean.com</span>`;
+  contactDiv.innerHTML = `<span><strong>To send your feedbacks about your experience in our restaurant: </strong></span><br /><br><span>myexperience@blueocean.com</span><br /><br><span><strong>For general information requests: </strong></span><br /><br><span>info@blueocean.com</span>`;
 
-  return contact_div;
+  return contactDiv;
 }
 
 export default Contact;
